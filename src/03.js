c2ca437ee8e7c656497611a776d7a58a3f7e03ce
David Valles <davidtjvalles@gmail.com>
2020-12-04 08:34:37 -0800

    Add day 3, part 2 solution

--- src/03.js
+++ src/03.js
@@ -1,15 +1,27 @@
-exports.part1 = (input) => {
+exports.part1 = (input, slope = { x: 3, y: 1 }) => {
   const w = input[0].length
   const h = input.length
-  const wFactor = Math.ceil((h * 3) / w)
+  const wFactor = Math.ceil((h * slope.x) / slope.y / w)
   const grid = input.map((row) => row.repeat(wFactor))
   let treeCount = 0
 
-  for (let row = 0, col = 0; row < h; row += 1, col += 3) {
+  for (let row = 0, col = 0; row < h; row += slope.y, col += slope.x) {
     if (grid[row][col] === '#') {
       treeCount += 1
     }
   }
 
   return treeCount
+}
+
+exports.part2 = (input) => {
+  return [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+  ]
+    .map((slope) => exports.part1(input, slope))
+    .reduce((r, count) => r * count)
 }
--- tests/03-test.js
+++ tests/03-test.js
@@ -1,10 +1,14 @@
 const test = require('ava')
 
-const { part1 } = require('../src/03')
+const { part1, part2 } = require('../src/03')
 const data = require('../data/03')
 
 const input = data.input.trim().split('\n')
 
 test('part 1', (t) => {
   t.is(part1(input), 181)
+})
+
+test('part 2', (t) => {
+  t.is(part2(input), 1260601650)
 })
