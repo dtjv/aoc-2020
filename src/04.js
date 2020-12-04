4fb716b4efd282be105fea97953fe3e99e16e196
David Valles <davidtjvalles@gmail.com>
2020-12-04 11:20:14 -0800

    Refactor day 4, part 1 solution

--- src/04.js
+++ src/04.js
@@ -1,13 +1,29 @@
-exports.part1 = (input) => {
+const checkRequiredFields = (passport) => {
   const requiredFields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt']
+  return requiredFields.every((field) => !!passport[field])
+}
 
+const format = (input) => {
   return input
+    .trim()
+    .split('\n\n')
+    .map((r) => r.replaceAll('\n', ' '))
     .map((passport) =>
       passport.split(' ').reduce((r, o) => {
         const [field, value] = o.split(':')
         return { ...r, [field]: value }
       }, {})
     )
-    .filter((passport) => requiredFields.every((field) => passport[field]))
-    .length
 }
+
+const validate = (...validationCallbacks) => {
+  return (input) => {
+    const data = format(input)
+    return data.filter((passport) =>
+      validationCallbacks.every((callback) => callback(passport))
+    ).length
+  }
+}
+
+exports.part1 = validate(checkRequiredFields)
+//exports.part2 = validate(checkRequiredFields, checkFieldValues)
--- tests/04-test.js
+++ tests/04-test.js
@@ -1,12 +1,7 @@
 const test = require('ava')
 
 const { part1 } = require('../src/04')
-const data = require('../data/04')
-
-const input = data.input
-  .trim()
-  .split('\n\n')
-  .map((record) => record.replaceAll('\n', ' '))
+const { input } = require('../data/04')
 
 test('part 1', (t) => {
   t.is(part1(input), 242)
