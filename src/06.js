1a5866b175de687010b41612f8d48a7aaeac9a44
David Valles <davidtjvalles@gmail.com>
2020-12-07 11:53:42 -0800

    Add day 6, part 2 solution

--- src/06.js
+++ src/06.js
@@ -12,3 +12,32 @@ exports.part1 = (input) => {
     })
     .reduce((total, group) => total + Object.keys(group).length, 0)
 }
+
+exports.part2 = (input) => {
+  const data = format(input).map((group) => group.split('\n'))
+  const numPeoplePerGroup = data.map((group) => group.length)
+
+  return data
+    .map((group) =>
+      group
+        .map((g) => g.split(''))
+        .reduce((hash, p) => {
+          p.forEach((answer) => {
+            if (!hash[answer]) {
+              hash[answer] = 0
+            }
+            hash[answer] += 1
+          })
+          return hash
+        }, {})
+    )
+    .reduce(
+      (count, group, idx) =>
+        count +
+        Object.values(group).reduce(
+          (r, v) => (v === numPeoplePerGroup[idx] ? r + 1 : r),
+          0
+        ),
+      0
+    )
+}
--- tests/06-test.js
+++ tests/06-test.js
@@ -1,9 +1,14 @@
 const test = require('ava')
 
-const { part1 } = require('../src/06')
+const { part1, part2 } = require('../src/06')
 const { dataset1, dataset2 } = require('../data/06')
 
 test('part 1', (t) => {
   t.is(part1(dataset1), 11)
   t.is(part1(dataset2), 6612)
+})
+
+test('part 2', (t) => {
+  t.is(part2(dataset1), 6)
+  t.is(part2(dataset2), 3268)
 })
