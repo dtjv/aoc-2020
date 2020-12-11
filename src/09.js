f1c74e50949227ebb3bb8ffe626f152012404d40
David Valles <davidtjvalles@gmail.com>
2020-12-11 07:52:46 -0800

    Add day 9, part 2 solution

--- src/09.js
+++ src/09.js
@@ -24,3 +24,35 @@ exports.part1 = (input) => {
 
   return t < data.length ? data[t] : -1
 }
+
+const sum = (numbers) => numbers.reduce((total, num) => total + num, 0)
+
+const locateSet = (data, target) => {
+  let d
+  let s = 0
+  let e = 2
+
+  while (e < data.length) {
+    d = data.slice(s, e)
+    let total = sum(d)
+
+    if (total < target) {
+      e += 1
+    } else if (total > target) {
+      s += 1
+      if (e - s < 2) {
+        e += 1
+      }
+    } else {
+      return d
+    }
+  }
+
+  return []
+}
+
+exports.part2 = (input, target) => {
+  const set = locateSet(format(input.data), target)
+  const sorted = set.sort((a, b) => a - b)
+  return sorted[0] + sorted[sorted.length - 1]
+}
--- tests/09-test.js
+++ tests/09-test.js
@@ -1,9 +1,14 @@
 const test = require('ava')
 
-const { part1 } = require('../src/09')
+const { part1, part2 } = require('../src/09')
 const { dataset1, dataset2 } = require('../data/09')
 
 test('part 1', (t) => {
   t.is(part1(dataset1), 127)
   t.is(part1(dataset2), 14144619)
+})
+
+test('part 2', (t) => {
+  t.is(part2(dataset1, 127), 62)
+  t.is(part2(dataset2, 14144619), 1766397)
 })
