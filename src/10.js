6d065bef88f86840444904b8d091bc6614347e89
David Valles <davidtjvalles@gmail.com>
2021-01-05 10:06:08 -0800

    Add day 10, part 2 attempt (wip)

--- src/10.js
+++ src/10.js
@@ -1,15 +1,14 @@
 const format = (input) => {
-  return input
+  const data = input
     .trim()
     .split('\n')
     .map((str) => parseInt(str))
     .sort((a, b) => a - b)
+
+  return [0, ...data, data[data.length - 1] + 3]
 }
 
-exports.part1 = (input) => {
-  let data = format(input)
-  data = [0, ...data, data[data.length - 1] + 3]
-
+const computeDiffs = (data) => {
   const result = {}
 
   for (let i = 1; i < data.length; i += 1) {
@@ -21,5 +20,32 @@ exports.part1 = (input) => {
     result[diff] += 1
   }
 
+  return result
+}
+
+/*
+const generateSequences = (data) => {
+  let result = []
+
+  for (let i = 1; i < data.length - 2; i += 1) {
+    if (data[i - 1] + 3 >= data[i + 1]) {
+      result.push(i)
+    }
+  }
+
+  return result
+}
+*/
+
+exports.part1 = (input) => {
+  const result = computeDiffs(format(input))
   return result[1] * result[3]
 }
+
+/*
+exports.part2 = (input) => {
+  let data = format(input)
+  //return Object.keys(generateSequences(data)).length
+  return generateSequences(data)
+}
+*/
