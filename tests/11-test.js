b8a169ffab37ce954dd928198c20d1dfaa4ac62c
David Valles <davidtjvalles@gmail.com>
2021-01-18 05:09:15 -0800

    Add test for day 11, part 1

---
+++ tests/11-test.js
@@ -0,0 +1,9 @@
+const test = require('ava')
+
+const { part1 } = require('../src/11')
+const { dataSetA, dataSetB } = require('../data/11')
+
+test('part 1', (t) => {
+  t.is(part1(dataSetA), 37)
+  t.is(part1(dataSetB), 2113)
+})
