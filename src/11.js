695585babeb92337cccba85d145dcd8025990081
David Valles <davidtjvalles@gmail.com>
2021-01-18 09:35:27 -0800

    Add day 11, part 2 solution

--- src/11.js
+++ src/11.js
@@ -11,39 +11,73 @@ const format = (data) => {
     .map((row) => row.split(''))
 }
 
-const processCell = (grid, g, row, col) => {
-  let changed = false
+const getCountForChar = (grid, char) => {
+  let count = 0
 
+  for (const row of grid) {
+    for (const cell of row) {
+      if (cell === char) count++
+    }
+  }
+
+  return count
+}
+
+// returns an array of visible cell values from cell at ['row','col']. a visible
+// cell is the first non-floor cell in any direction. adjacent cells are only
+// those that touch cell at ['row','col'].
+const getVisibleCells = (grid, row, col, options) => {
+  const adjacentOnly = !!options?.adjacentOnly
+  const steps = [-1, 0, 1]
   const rows = grid.length
   const cols = grid[0].length
+  const visibleCells = []
 
-  const adjacentCells = [
-    { r: row - 1, c: col },
-    { r: row - 1, c: col + 1 },
-    { r: row, c: col + 1 },
-    { r: row + 1, c: col + 1 },
-    { r: row + 1, c: col },
-    { r: row + 1, c: col - 1 },
-    { r: row, c: col - 1 },
-    { r: row - 1, c: col - 1 },
-  ]
-    .filter(({ r, c }) => r >= 0 && r < rows && c >= 0 && c < cols)
-    .map(({ r, c }) => grid[r][c])
+  for (let i = 0; i < steps.length; i++) {
+    for (let j = 0; j < steps.length; j++) {
+      let r = row + steps[i]
+      let c = col + steps[j]
 
+      while (r >= 0 && c >= 0 && r < rows && c < cols) {
+        if (r === row && c === col) break
+
+        // we only care about the FIRST cell that's not the floor.
+        if (grid[r][c] !== Cell.FLOOR) {
+          visibleCells.push(grid[r][c])
+          break
+        }
+
+        if (adjacentOnly) break
+
+        r = r + steps[i]
+        c = c + steps[j]
+      }
+    }
+  }
+
+  return visibleCells
+}
+
+const processCell = (grid, g, row, col, options) => {
+  const visibleCells = getVisibleCells(grid, row, col, options)
+  let changed = false
+
+  // if cell is empty and all adjacent cells are empty, then occupy cell
   if (
     grid[row][col] === Cell.EMPTY &&
-    adjacentCells.every((cell) => cell !== Cell.OCCUPIED)
+    visibleCells.every((cell) => cell !== Cell.OCCUPIED)
   ) {
     g[row][col] = Cell.OCCUPIED
     changed = true
   }
 
+  // if cell and N+ adjacent cells are occupied, empty the cell
   if (
     grid[row][col] === Cell.OCCUPIED &&
-    adjacentCells.reduce(
+    visibleCells.reduce(
       (count, cell) => (cell === Cell.OCCUPIED ? count + 1 : count),
       0
-    ) >= 4
+    ) >= options.occupiedThreshold
   ) {
     g[row][col] = Cell.EMPTY
     changed = true
@@ -52,13 +86,15 @@ const processCell = (grid, g, row, col) => {
   return changed
 }
 
-const processGrid = (grid) => {
+// modifies grid and returns true if grid changed, false otherwise.
+const processGrid = (grid, options) => {
   const g = JSON.parse(JSON.stringify(grid))
+
   let changed = false
 
   for (let i = 0; i < g.length; i++) {
     for (let j = 0; j < g[i].length; j++) {
-      if (processCell(grid, g, i, j)) {
+      if (processCell(grid, g, i, j, options)) {
         changed = true
       }
     }
@@ -73,21 +109,30 @@ const processGrid = (grid) => {
   return changed
 }
 
+//------------------------------------------------------------------------------
+// part 1
+//------------------------------------------------------------------------------
 exports.part1 = (input) => {
   const grid = format(input)
-  let changed = processGrid(grid)
+  let changed = true
 
   while (changed) {
-    changed = processGrid(grid)
+    changed = processGrid(grid, { adjacentOnly: true, occupiedThreshold: 4 })
   }
 
-  let count = 0
+  return getCountForChar(grid, Cell.OCCUPIED)
+}
 
-  for (const row of grid) {
-    for (const cell of row) {
-      if (cell === Cell.OCCUPIED) count++
-    }
+//------------------------------------------------------------------------------
+// part 2
+//------------------------------------------------------------------------------
+exports.part2 = (input) => {
+  const grid = format(input)
+  let changed = true
+
+  while (changed) {
+    changed = processGrid(grid, { adjacentOnly: false, occupiedThreshold: 5 })
   }
 
-  return count
+  return getCountForChar(grid, Cell.OCCUPIED)
 }
--- tests/11-test.js
+++ tests/11-test.js
@@ -1,9 +1,14 @@
 const test = require('ava')
 
-const { part1 } = require('../src/11')
+const { part1, part2 } = require('../src/11')
 const { dataSetA, dataSetB } = require('../data/11')
 
 test('part 1', (t) => {
   t.is(part1(dataSetA), 37)
   t.is(part1(dataSetB), 2113)
+})
+
+test('part 2', (t) => {
+  t.is(part2(dataSetA), 26)
+  t.is(part2(dataSetB), 1865)
 })
